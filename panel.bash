d912980dd306885f52fb63d59274beeaa83fcf41
Nathan <nathanvandijk1998@gmail.com>
2022-06-26 23:58:39 +0200

    Updaate files
    update files

--- panel.bash
+++ panel.bash
@@ -1,3 +1,33 @@
+#!/bin/bash
+set -e
+
+###########################################################################################
+#                                                                                         #
+# Project 'unofficail-pterodactyl-install' for panel                                      #
+#                                                                                         #
+# Copyright (C) 2018 - 2022, Nathan van Dijk, <info@nathanvandijk.nl>                     #
+#                                                                                         #
+#   This program is free software: you can redistribute it and/or modify                  #
+#   it under the terms of the GNU General Public License as published by                  #
+#   the Free Software Foundation, either version 3 of the License, or                     #
+#   (at your option) any later version.                                                   #
+#                                                                                         #
+#   This program is distributed in the hope that it will be useful,                       #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of                        #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                         #
+#   GNU General Public License for more details.                                          #
+#                                                                                         #
+#   You should have received a copy of the GNU General Public License                     #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.                #
+#                                                                                         #
+# https://github.com/NathantheDev/unofficail-pterodactyl-install/blob/master/LICENSE      #
+#                                                                                         #
+# This script is not associated with the official Pterodactyl Project.                    #
+# https://github.com/NathantheDev/unofficail-pterodactyl-install                          #
+#                                                                                         #
+###########################################################################################
+
+
 apt update
 apt upgrade -y 
 
